803e37658cb0cb342bd6c62d235cc4e7c0cb99bf
SycWannaSleep <58065914+milkandpotato@users.noreply.github.com>
2024-11-23 13:03:31 +0800

    feat：集成swagger

--- ScratchCard.Web/Program.cs
+++ ScratchCard.Web/Program.cs
@@ -1,7 +1,16 @@
 using ScratchCard.Web.Components;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.Swagger;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var config = builder.Configuration;
+
+var services = builder.Services;
+
+services.AddControllers();
+services.AddSwaggerGen();
+services.AddEndpointsApiExplorer();
 //使用antDesign
 builder.Services.AddAntDesign();
 
@@ -18,11 +27,17 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+else
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 app.UseAntiforgery();
+app.MapControllers();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
